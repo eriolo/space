457cae1bd79f9b33ba288014b0321121362a23da
Erik Olofsson <erik.olofsson@proxy.com>
2023-01-05 11:53:48 +0100

    chore: Add eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import logo from './logo.png';
-import './App.css';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client'
+import logo from './logo.png'
+import './App.css'
+
 const customerQuery = gql`
   query customerData {
     company {
-        employees
-        launch_sites
-        name
-        summary
-        test_sites
-        valuation
-        vehicles
-      }
+      employees
+      launch_sites
+      name
+      summary
+      test_sites
+      valuation
+      vehicles
+    }
   }
-`;
+`
 
 function App() {
-  const { loading, error, data } = useQuery(customerQuery);
-  if (loading) return <p>Loading... 🚀 </p>;
-  if (error) return <p>Error 😢</p>;
+  const { loading, error, data } = useQuery(customerQuery)
+  if (loading) return <p>Loading... 🚀 </p>
+  if (error) return <p>Error 😢</p>
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Customer data</p>
-        <p>{JSON.stringify(data,null,100)}</p>
+        <p>{JSON.stringify(data, null, 100)}</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import App from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 const client = new ApolloClient({
   uri: 'https://api.spacex.land/graphql',
-  cache: new InMemoryCache()
-});
+  cache: new InMemoryCache(),
+})
 
 root.render(
   <React.StrictMode>
-  <ApolloProvider client={client}>
-  <App />
-  </ApolloProvider>
-</React.StrictMode>
-);
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+)
